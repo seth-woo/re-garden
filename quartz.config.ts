b5084e8bb022ebba5b5a0fc59e3f541d91b9108c
Seth Woo <sethwoo555@gmail.com>
2026-04-04 16:03:35 +0800

    test ghost white

--- quartz.config.ts
+++ quartz.config.ts
@@ -33,7 +33,7 @@ const config: QuartzConfig = {
       colors: {
         lightMode: {
           // light: "#faf8f8",
-          light: "#f9f9f7",
+          light: "#F8F8FF",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
